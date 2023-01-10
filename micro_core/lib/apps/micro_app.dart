fcfc04051634e331ec7928d89652a1fcdf701a6b
RenanLopes <renan.lopes.contato@gmail.com>
2023-01-10 12:19:27 -0300

    removing micro_apps to split into repositories

--- micro_core/lib/apps/micro_app.dart
+++
@@ -1,7 +0,0 @@
-import 'package:micro_core/apps/micro_core_utils.dart';
-
-abstract class MicroApp {
-  String get microAppName;
-
-  Map<String, WidgetBuilderArgs> get routes;
-}
--- micro_core/lib/micro_core.dart
+++
@@ -1,3 +0,0 @@
-export './apps/base_app.dart';
-export './apps/micro_app.dart';
-export './apps/micro_core_utils.dart';
